26c4afaf1c2d956c0ec1e6de7008ebd48cd020b7
Dave Schlauderaff <dschlaud@gmail.com>
2019-08-14 21:00:04 -0500

    user creation with token working

--- app/auth/service.js
+++ app/auth/service.js
@@ -1,85 +1,93 @@
-import Service from '@ember/service';
-import { inject as service } from '@ember/service'
-import { computed } from '@ember/object'
-import signInUserMutation from 'hackernews-ember-apollo/gql/mutations/signInUser'
-import createUser from 'hackernews-ember-apollo/gql/mutations/createUser'
+import Service from "@ember/service";
+import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
+import signInUserMutation from "hackernews-ember-apollo/gql/mutations/signInUser";
+import createUser from "hackernews-ember-apollo/gql/mutations/createUser";
 
-const GC_USER_ID = 'graphcool-user-id';
-const GC_AUTH_TOKEN = 'graphcool-auth-token'
+const GC_USER_ID = "graphcool-user-id";
+const GC_AUTH_TOKEN = "graphcool-auth-token";
 
 export default Service.extend({
   init() {
-    this._super(...arguments)
-    this.getUserId()
-    this.getAuthToken()
+    this._super(...arguments);
+    this.getUserId();
+    this.getAuthToken();
   },
 
   apollo: service(),
   authToken: null,
   userId: null,
 
-  isLoggedIn: computed('userId', function() {
-    return !!this.userId
+  isLoggedIn: computed("userId", function() {
+    return !!this.userId;
   }),
 
   async loginOrSignUp(state, name, email, password) {
-    let variables
+    const apollo = this.get("apollo");
+    let variables;
     try {
-      if(state){
-        variables = { email, password }
-        const result = await this.apollo.mutate({ mutation: signInUserMutation, variables }, 'signInUser')
-        this.saveUserData(result.user.id, result.token)
+      if (state) {
+        variables = { email, password };
+        const result = await apollo.mutate({
+          mutation: signInUserMutation,
+          variables
+        });
+        this.saveUserData(result.user.id, result.token);
       } else {
-        variables = { email, password }
-        const result = await this.apollo.mutate({ mutation: createUser, variables }, 'signInUser').toPromise()
-        this.saveUserData(result.user.id, result.token)
+        variables = { name, email, password };
+        const result = await apollo.mutate({
+          mutation: createUser,
+          variables
+        });
+
+        debugger
+        const { signupUser } = result;
+        this.saveUserData(signupUser.id, signupUser.token);
       }
+    } catch (error) {
+      console.log(error);
     }
-    catch(error) {
-      console.log(error)
-    } 
   },
 
   async logout() {
-    await this.removeUserId()
-    await this.removeAuthToken()
+    await this.removeUserId();
+    await this.removeAuthToken();
   },
 
   saveUserData(id, token) {
-    debugger
-    this.setUserId(id)
-    this.setAuthToken(token)
+    this.setUserId(id);
+    this.setAuthToken(token);
   },
 
   getUserId() {
-    const userId = localStorage.getItem(GC_USER_ID)
-    this.setUserId(userId)
-    return userId
+    const userId = localStorage.getItem(GC_USER_ID);
+    this.setUserId(userId);
+    return userId;
   },
 
   getAuthToken() {
-    const token = localStorage.getItem(GC_AUTH_TOKEN)
-    this.setAuthToken(token)
-    return token
+    const token = localStorage.getItem(GC_AUTH_TOKEN);
+    this.setAuthToken(token);
+    return token;
   },
 
   removeUserId() {
-    localStorage.removeItem(GC_USER_ID)
-    this.set('userId', null)
+    localStorage.removeItem(GC_USER_ID);
+    this.set("userId", null);
   },
 
   removeAuthToken() {
-    localStorage.removeItem(GC_AUTH_TOKEN)
-    this.set('authToken', null)
+    localStorage.removeItem(GC_AUTH_TOKEN);
+    this.set("authToken", null);
   },
 
   setUserId(id) {
-    localStorage.setItem(GC_USER_ID, id)
-    this.set('userId', id)
+    localStorage.setItem(GC_USER_ID, id);
+    this.set("userId", id);
   },
 
   setAuthToken(token) {
-    localStorage.setItem(GC_AUTH_TOKEN, token)
-    this.set('authToken', token)
+    localStorage.setItem(GC_AUTH_TOKEN, token);
+    this.set("authToken", token);
   }
 });
--- app/login/controller.js
+++ app/login/controller.js
@@ -1,26 +1,26 @@
-import Controller from '@ember/controller';
-import { inject as service } from '@ember/service'
+import Controller from "@ember/controller";
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
   auth: service(),
   loginState: true,
-  
+
   actions: {
     changeLoginState() {
-      this.toggleProperty('loginState')
+      this.toggleProperty("loginState");
     },
 
     async loginOrSignUp() {
-      const loginState = this.loginState
-      const email = this.email
-      const name = this.name
-      const password = this.password
+      const loginState = this.loginState;
+      const email = this.email;
+      const name = this.name;
+      const password = this.password;
 
       try {
-        await this.auth.loginOrSignUp(loginState, name, email, password)
-        this.transitionToRoute('/')
-      } catch(error) {
-        alert(error)
+        await this.auth.loginOrSignUp(loginState, name, email, password);
+        this.transitionToRoute("/");
+      } catch (error) {
+        console.log("controller", error);
       }
     }
   }
